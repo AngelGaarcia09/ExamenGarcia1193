8e1f2841e10c86c2ddf9d272950680c2936e2dd9
ANGEL_MISAEL_GARCIA_HERNANDEZ <a.22308051281193@cbtis128.edu.mx>
2025-03-13 18:50:19 +0000

    denuevo

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,100 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
+
+class RegistroDeTroca {
+  final String codigoTroca;
+  final String modelo;
+  final String marca;
+  final String tipoLlantas;
+  final double precio;
+  final String color;
+
+  RegistroDeTroca({
+    required this.codigoTroca,
+    required this.modelo,
+    required this.marca,
+    required this.tipoLlantas,
+    required this.precio,
+    required this.color,
+  });
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+  final List<Map<String, dynamic>> troca = [
+    {
+      'CodigoTroca': '001',
+      'Modelo': 'F-150',
+      'Marca': 'Ford',
+      'TipoLlantas': 'All-Terrain',
+      'Precio': 35000.0,
+      'Color': 'Rojo',
+    },
+    {
+      'CodigoTroca': '002',
+      'Modelo': 'Silverado',
+      'Marca': 'Chevrolet',
+      'TipoLlantas': 'Highway',
+      'Precio': 40000.0,
+      'Color': 'Azul',
+    },
+  ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+      debugShowCheckedModeBanner:
+          false, // Quita la barra de debug de la esquina derecha
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('     Registro Troca Garcia:1193'), // Título de la AppBar
+          backgroundColor: Color(0x8439e85f), // Color de fondo de la AppBar
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: troca.map((data) {
+              // Asigna colores diferentes a cada contenedor
+              final containerColor = data['CodigoTroca'] == '001'
+                  ? Color(0xff1ad9d9)
+                  : Color(0xff1886ec);
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+              return Container(
+                margin: EdgeInsets.all(10),
+                padding: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  color: containerColor, // Color diferente para cada contenedor
+                  borderRadius: BorderRadius.circular(15),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(1),
+                      spreadRadius: 2,
+                      blurRadius: 10,
+                      offset: Offset(0, 9),
+                    ),
+                  ],
+                ),
+                child: Column(
+                  children: [
+                    Text('Código: ${data['CodigoTroca']}',
+                        style: TextStyle(fontSize: 18)),
+                    Text('Modelo: ${data['Modelo']}',
+                        style: TextStyle(fontSize: 18)),
+                    Text('Marca: ${data['Marca']}',
+                        style: TextStyle(fontSize: 18)),
+                    Text('Tipo de Llantas: ${data['TipoLlantas']}',
+                        style: TextStyle(fontSize: 18)),
+                    Text('Precio: \$${data['Precio']}',
+                        style: TextStyle(fontSize: 18)),
+                    Text('Color: ${data['Color']}',
+                        style: TextStyle(fontSize: 18)),
+                  ],
+                ),
+              );
+            }).toList(),
+          ),
         ),
       ),
     );
